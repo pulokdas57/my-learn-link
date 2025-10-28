adb1debbb4d8b2ab758e86a9ea7aff7d354148aa
pulokdas57 <harshdassahil@gmail.com>
2025-10-28 10:27:10 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/my-learn-link",
   server: {
     host: "::",
     port: 8080,
